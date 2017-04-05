181afa409cc0c7a3a771a900b7d180dd7c4550b9
frenchu <frenchu@o2.pl>
2017-04-05 22:41:07 +0200

    Changed unit test. * Calling buildQuery() method in a static way. * Removed mockito dependency.

--- src/test/java/JsqParserIntroductionTest.java
+++ src/test/java/JsqParserIntroductionTest.java
@@ -1,5 +1,4 @@
 import org.junit.Test;
-import org.mockito.Mock;
 
 import static org.junit.Assert.assertEquals;
 
@@ -7,11 +6,8 @@ public class JsqParserIntroductionTest {
 
     private String query = "SELECT report_id FROM report WHERE NAV > @NavGUI AND status = 'IN_PROGRESS'";
 
-    @Mock
-    private JsqlParserIntroduction jsqlParserIntroduction;
-
     @Test
     public void verifySQLBuilder(){
-        assertEquals(query,jsqlParserIntroduction.buildQuery());
+        assertEquals(query, JsqlParserIntroduction.buildQuery());
     }
 }
